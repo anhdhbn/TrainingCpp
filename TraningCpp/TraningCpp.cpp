8f8e70fc4158fcabad9cea7ef7c823a937e566ea
anhdh <anhdhbn@gmail.com>
2018-04-17 12:28:27 +0700

    finish

--- TraningCpp/TraningCpp.cpp
+++ TraningCpp/TraningCpp.cpp
@@ -14,9 +14,39 @@
 #include <queue>
 #include <stack>
 
-int main()
-{
+using namespace std;
 
-    return 0;
+bool Check(string str1, string str2)
+{
+	if (str1.length() == str2.length())
+	{
+		for (int i = 0; i < str1.length(); i++)
+		{
+			if (str1[i] != str2[i])
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
+int main() 
+{
+	string str1, str2;
+	int n;
+	cin >> n;
+	cin.ignore();
+	for (int i = 0; i < n; i++)
+	{
+		cin >> str1 >> str2;
+		sort(str1.begin(), str1.end());
+		sort(str2.begin(), str2.end());
+		cout << (Check(str1, str2) ? "yes" : "no") << endl;
+	}
+	return 0;
+}
